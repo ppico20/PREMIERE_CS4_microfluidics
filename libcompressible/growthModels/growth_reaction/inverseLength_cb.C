f85bf76d0286097e91afbb372e1539e205e91818
Paula Pico <pdp19@ce-pdp19.ce.ic.ac.uk>
2021-09-01 21:53:29 +0100

    added new library for compressible solver with nucleation model. Updated the case

---
+++ libcompressible/growthModels/growth_reaction/inverseLength_cb.C
@@ -0,0 +1,309 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | OpenQBMM - www.openqbmm.org
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2019 Alberto Passalacqua
+-------------------------------------------------------------------------------
+License
+    This file is derivative work of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "growth_reaction.H"
+#include "addToRunTimeSelectionTable.H"
+#include "fundamentalConstants.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace populationBalanceSubModels
+{
+namespace growthModels
+{
+    defineTypeNameAndDebug(growth_reaction, 0);
+
+    addToRunTimeSelectionTable
+    (
+        growthModel,
+        growth_reaction,
+        dictionary
+    );
+}
+}
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::populationBalanceSubModels::growthModels::growth_reaction
+::growth_reaction
+(
+    const dictionary& dict,
+    const fvMesh& mesh
+)
+:
+    growthModel(dict, mesh),
+
+    continuousPhase_(dict.lookupOrDefault("continuousPhase", word::null)),
+    
+    c_eq_("c_eq", dimMoles*pow(dimLength,-3.0), dict)
+
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::populationBalanceSubModels::growthModels::growth_reaction::
+~growth_reaction()
+{}
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+
+Foam::scalar
+Foam::populationBalanceSubModels::growthModels::growth_reaction::phaseSpaceConvection
+(
+    const labelList& momentOrder,
+    const label celli,
+    const scalarQuadratureApproximation& quadrature
+)
+{
+    scalar gSource(0);
+
+    const volScalarField& cb = mesh_.lookupObject<volScalarField>("c");
+    
+    scalar S = cb[celli]/c_eq_.value();
+
+    const PtrList<volScalarNode>& nodes = quadrature.nodes();
+    label sizeIndex = nodes[0].sizeIndex();
+
+    if (sizeIndex == -1)
+    {
+        return gSource;
+    }
+    label sizeOrder = momentOrder[sizeIndex];
+
+
+    bool lengthBased = nodes[0].lengthBased();
+    bool volumeFraction = nodes[0].useVolumeFraction();
+    if (volumeFraction)
+    {
+        if (lengthBased)
+        {
+            sizeOrder += 3;
+        }
+        else
+        {
+            sizeOrder += 1;
+        }
+    }
+
+    if (sizeOrder < 1)
+    {
+        return gSource;
+    }
+
+    const labelList& scalarIndexes = nodes[0].scalarIndexes();
+
+    if (!nodes[0].extended())
+    {
+        forAll(nodes, pNodeI)
+        {
+            const volScalarNode& node = nodes[pNodeI];
+
+            scalar bAbscissa =
+                max(node.primaryAbscissae()[sizeIndex][celli], scalar(0));
+            scalar d = node.d(celli, bAbscissa);
+            scalar n =
+                node.n(celli, node.primaryWeight()[celli], bAbscissa);
+
+            scalar gSourcei =
+                n
+               *(Kg(d, lengthBased)*(S-1.0))
+               *sizeOrder
+               *pow(bAbscissa, sizeOrder - 1);
+
+            forAll(scalarIndexes, nodei)
+            {
+                if (scalarIndexes[nodei] != sizeIndex)
+                {
+                    gSourcei *=
+                        pow
+                        (
+                            node.primaryAbscissae()[nodei][celli],
+                            momentOrder[scalarIndexes[nodei]]
+                        );
+                }
+            }
+            gSource += gSourcei;
+        }
+
+        return gSource;
+    }
+
+    forAll(nodes, pNodeI)
+    {
+        const volScalarNode& node = nodes[pNodeI];
+
+        forAll(node.secondaryWeights()[sizeIndex], sNodei)
+        {
+            scalar bAbscissa =
+                max
+                (
+                    node.secondaryAbscissae()[sizeIndex][sNodei][celli],
+                    scalar(0)
+                );
+            scalar d = node.d(celli, bAbscissa);
+            scalar n =
+                node.n(celli, node.primaryWeight()[celli], bAbscissa)
+               *node.secondaryWeights()[sizeIndex][sNodei][celli];
+
+            scalar gSourcei =
+                n
+               *(Kg(d, lengthBased)*(S-1.0))
+               *sizeOrder
+               *pow(bAbscissa, sizeOrder - 1);
+
+            forAll(scalarIndexes, cmpt)
+            {
+                if (scalarIndexes[cmpt] != sizeIndex)
+                {
+                    gSourcei *=
+                        node.secondaryWeights()[cmpt][sNodei][celli]
+                       *pow
+                        (
+                            node.secondaryAbscissae()[cmpt][sNodei][celli],
+                            momentOrder[scalarIndexes[cmpt]]
+                        );
+                }
+            }
+            gSource += gSourcei;
+        }
+    }
+
+    return gSource;
+}
+
+
+
+Foam::scalar
+Foam::populationBalanceSubModels::growthModels::growth_reaction::phaseSpaceConvection
+(
+    const labelList& momentOrder,
+    const label celli,
+    const velocityQuadratureApproximation& quadrature
+)
+{
+    scalar gSource(0);
+
+    const PtrList<volVelocityNode>& nodes = quadrature.nodes();
+    label sizeIndex = nodes[0].sizeIndex();
+
+    if (sizeIndex == -1)
+    {
+        return gSource;
+    }
+    label sizeOrder = momentOrder[sizeIndex];
+
+    bool lengthBased = nodes[0].lengthBased();
+    bool volumeFraction = nodes[0].useVolumeFraction();
+    if (volumeFraction)
+    {
+        if (lengthBased)
+        {
+            sizeOrder += 3;
+        }
+        else
+        {
+            sizeOrder += 1;
+        }
+    }
+
+    if (sizeOrder < 1)
+    {
+        return gSource;
+    }
+
+    const labelList& scalarIndexes = nodes[0].scalarIndexes();
+    const labelList& velocityIndexes = nodes[0].velocityIndexes();
+
+    forAll(nodes, pNodeI)
+    {
+        const volVelocityNode& node = nodes[pNodeI];
+
+        scalar bAbscissa =
+            max(node.primaryAbscissae()[sizeIndex][celli], scalar(0));
+        scalar d = node.d(celli, bAbscissa);
+        scalar n =
+            node.n(celli, node.primaryWeight()[celli], bAbscissa);
+
+        scalar gSourcei =
+            n*Kg(d, lengthBased)*sizeOrder*pow(bAbscissa, sizeOrder - 1);
+
+        forAll(scalarIndexes, nodei)
+        {
+            if (scalarIndexes[nodei] != sizeIndex)
+            {
+                gSourcei *=
+                    pow
+                    (
+                        node.primaryAbscissae()[nodei][celli],
+                        momentOrder[scalarIndexes[nodei]]
+                    );
+            }
+        }
+        forAll(velocityIndexes, cmpt)
+        {
+            gSourcei *=
+                pow
+                (
+                    node.velocityAbscissae()[celli][cmpt],
+                    momentOrder[velocityIndexes[cmpt]]
+                );
+        }
+        gSource += gSourcei;
+    }
+
+    return gSource;
+}
+
+
+Foam::scalar
+Foam::populationBalanceSubModels::growthModels::growth_reaction::Kg
+(
+    const scalar& d,
+    const bool lengthBased,
+    const label environment
+) const
+{
+
+    if(d < SMALL){
+ 
+    return 0.0;
+
+    } else {
+
+     return (Cg_.value()/d);
+    }
+
+    //return (Cg_.value()/max(d,dmin_.value()));
+}
+
+// ************************************************************************* //
---
+++ libcompressible/nucleationModels/nucleation_reaction/nucleation_reaction.C
@@ -0,0 +1,105 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | OpenQBMM - www.openqbmm.org
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Code created 2016-2018 by Alberto Passalacqua
+    Contributed 2018-07-31 to the OpenFOAM Foundation
+    Copyright (C) 2018 OpenFOAM Foundation
+    Copyright (C) 2019-2020 Alberto Passalacqua
+-------------------------------------------------------------------------------
+License
+    This file is derivative work of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "nucleation_reaction.H"
+#include "addToRunTimeSelectionTable.H"
+#include "physicoChemicalConstants.H"
+
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace populationBalanceSubModels
+{
+namespace nucleationModels
+{
+    defineTypeNameAndDebug(nucleation_reaction, 0);
+
+    addToRunTimeSelectionTable
+    (
+        nucleationModel,
+        nucleation_reaction,
+        dictionary
+    );
+}
+}
+}
+
+//using Foam::constant::physicoChemical::NA;
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::populationBalanceSubModels::nucleationModels::nucleation_reaction::nucleation_reaction
+(
+    const dictionary& dict,
+    const fvMesh& mesh
+)
+:
+    nucleationModel(dict, mesh),
+
+    continuousPhase_(dict.lookupOrDefault("continuousPhase", word::null)),
+
+    k1_("k1", pow(dimTime,-1), dict),
+    dcrit_("dcrit", dimLength, dict),
+    mwAgs_("mwAgs", dimMass*pow(dimMoles,-1.0), dict)
+    
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::populationBalanceSubModels::nucleationModels::nucleation_reaction::~nucleation_reaction()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+Foam::scalar
+Foam::populationBalanceSubModels::nucleationModels::nucleation_reaction::nucleationSource
+(
+    const label& momentOrder,
+    const label celli,
+    const label environment
+) const
+{
+    
+
+    const volScalarField& y_Agl = mesh_.lookupObject<volScalarField>("y_Agl");
+    const volScalarField& rho = mesh_.lookupObject<volScalarField>("rho");
+
+    scalar abscissaNucleation = dcrit_.value();
+
+    return (6.0221417930e+23/mwAgs_.value())*(mwAgs_.value()*k1_.value()*(rho[celli]*y_Agl[celli]/mwAgs_.value()))*pow(abscissaNucleation, momentOrder);
+
+    //return Cn1_.value()*exp(-Cn2_.value()*pow(log(S),-2.0))*pow(abscissaNucleation, momentOrder);
+}
+
+// ************************************************************************* //
