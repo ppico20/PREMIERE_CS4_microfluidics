1a1dadc7fa0cfaf5a13525f66777cabed14bd576
Paula Pico <pdp19@ce-pdp19.ce.ic.ac.uk>
2022-07-04 14:38:13 +0100

    Modified PBM solver to include linearisation of source term and option to change stoichiometric coefficient of reduction reaction

--- applications/solvers/newReacting_buoyantPimpleFoam/newReacting_buoyantPbePimpleFoam.C
+++ applications/solvers/newReacting_buoyantPimpleFoam/newReacting_buoyantPbePimpleFoam.C
@@ -2,10 +2,14 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | www.openfoam.com
+    \\  /    A nd           | OpenQBMM - www.openqbmm.org
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
     Copyright (C) 2011-2017 OpenFOAM Foundation
+    Copyright (C) 2019-2020 Alberto Passalacqua
+-------------------------------------------------------------------------------
+2015-06-21 Alberto Passalacqua: Derived solver from buoyantPimpleFoam and
+                                added solution of population balance.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -24,14 +28,11 @@ License
     along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
 
 Application
-    newReacting_buoyantPimpleFoam
-
-Group
-    grpHeatTransferSolvers
+    NewReacting_buoyantPbePimpleFoam
 
 Description
-    Transient solver for buoyant, turbulent flow of compressible fluids for
-    ventilation and heat-transfer.
+    Transient solver for buoyant, turbulent flow of compressible fluids with
+    solution of population balance.
 
     Turbulence is modelled using a run-time selectable compressible RAS or
     LES model.
@@ -45,6 +46,7 @@ Description
 #include "fvOptions.H"
 #include "pimpleControl.H"
 #include "pressureControl.H"
+#include "populationBalanceModel.H"
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
@@ -52,8 +54,8 @@ int main(int argc, char *argv[])
 {
     argList::addNote
     (
-        "Transient solver for buoyant, turbulent fluid flow"
-        " of compressible fluids, including radiation."
+        "Transient solver for with population balance with buoyancy and "
+        "heat transfer."
     );
 
     #include "postProcess.H"
@@ -74,7 +76,7 @@ int main(int argc, char *argv[])
 
     // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
-    Info<< "\nStarting time loop\n" << endl;
+Info<< "\nStarting time loop\n" << endl;
 
     while (runTime.run())
     {
@@ -106,7 +108,7 @@ int main(int argc, char *argv[])
             }
         }
 
-        rho = thermo.rho();
+       rho = thermo.rho();
 
         volScalarField& y_AgNO3 = mesh.lookupObjectRef<volScalarField>("y_AgNO3");
         volScalarField& y_reduc = mesh.lookupObjectRef<volScalarField>("y_reduc");
@@ -114,44 +116,53 @@ int main(int argc, char *argv[])
         volScalarField& y_Ags = mesh.lookupObjectRef<volScalarField>("y_Ags");
         volScalarField& y_H2O = mesh.lookupObjectRef<volScalarField>("y_H2O");
 
+        #include "ySuSp.H"
+
         fvScalarMatrix y_AgNO3Eqn
         (
             fvm::ddt(rho,y_AgNO3)
             + fvm::div(phi, y_AgNO3)
-            - fvm::laplacian(rho*D_AgNO3, y_AgNO3) == - (mw_AgNO3)*kr*(rho*y_AgNO3/mw_AgNO3)*(rho*y_reduc/mw_reduc)
+            - fvm::laplacian(rho*D_AgNO3, y_AgNO3) == Su_AgNO3 + fvm::Sp(Sp_AgNO3, y_AgNO3)
         );
 
         fvScalarMatrix y_reducEqn
         (
             fvm::ddt(rho,y_reduc)
             + fvm::div(phi, y_reduc)
-            - fvm::laplacian(rho*D_reduc, y_reduc) == - (mw_reduc)*kr*(rho*y_AgNO3/mw_AgNO3)*(rho*y_reduc/mw_reduc)
+            - fvm::laplacian(rho*D_reduc, y_reduc) == Su_reduc + fvm::Sp(Sp_reduc, y_reduc)
         );
 
         fvScalarMatrix y_AglEqn
         (
             fvm::ddt(rho,y_Agl)
             + fvm::div(phi, y_Agl)
-            - fvm::laplacian(rho*D_Agl, y_Agl) == (mw_Agl)*(kr*(rho*y_AgNO3/mw_AgNO3)*(rho*y_reduc/mw_reduc)-k1*(rho*y_Agl/mw_Agl)-k2*(rho*y_Agl/mw_Agl)*(rho*y_Ags/mw_Ags))
+            - fvm::laplacian(rho*D_Agl, y_Agl) == Su_Agl + fvm::Sp(Sp_Agl, y_Agl)
         );
 
         fvScalarMatrix y_AgsEqn
         (
             fvm::ddt(rho,y_Ags)
             + fvm::div(phi, y_Ags)
-            - fvm::laplacian(rho*D_Ags, y_Ags) == (mw_Ags)*(k1*(rho*y_Agl/mw_Agl)-k2*(rho*y_Agl/mw_Agl)*(rho*y_Ags/mw_Ags))
+            - fvm::laplacian(rho*D_Ags, y_Ags) == Su_Ags + fvm::Sp(Sp_Ags, y_Ags)
         );
 
         fvScalarMatrix y_Ags2Eqn
         (
             fvm::ddt(rho,y_Ags2)
             + fvm::div(phi, y_Ags2)
-            - fvm::laplacian(rho*D_Ags2, y_Ags2) == (mw_Ags2)*(k2*(rho*y_Agl/mw_Agl)*(rho*y_Ags/mw_Ags))
+            - fvm::laplacian(rho*D_Ags2, y_Ags2) == Su_Ags2 + fvm::Sp(Sp_Ags2, y_Ags2)
         );
 
-        rate_product_Agl_reduction = (mw_Agl)*(kr*(rho*y_AgNO3/mw_AgNO3)*(rho*y_reduc/mw_reduc));
+        rate_product_Agl_reduction = (mw_Agl)*(kr*(pow(rho*y_AgNO3/mw_AgNO3,nu_AgNO3))*(pow(rho*y_reduc/mw_reduc,nu_reduc)));
         rate_product_Ags_nuc = (mw_Ags)*(k1*(rho*y_Agl/mw_Agl));
         rate_product_Ags2_g = (mw_Ags2)*(k2*(rho*y_Agl/mw_Agl)*(rho*y_Ags/mw_Ags));
+
+        y_AgNO3.relax();
+        y_reduc.relax();
+        y_Agl.relax();
+        y_Ags.relax();
+        y_Ags2.relax();
+        y_H2O.relax();
 
         y_AgNO3Eqn.solve();
         y_reducEqn.solve();
@@ -161,8 +172,22 @@ int main(int argc, char *argv[])
 
         y_H2O = scalar(1) - (y_AgNO3+y_reduc+y_Agl+y_Ags+y_Ags2);
 
-        runTime.write();
+        y_AgNO3.min(1.0);
+        y_AgNO3.max(0.0);
+        y_reduc.min(1.0);
+        y_reduc.max(0.0);
+        y_Agl.min(1.0);
+        y_Agl.max(0.0);
+        y_Ags.min(1.0);
+        y_Ags.max(0.0);
+        y_Ags2.min(1.0);
+        y_Ags2.max(0.0);
+        y_H2O.min(1.0);
+        y_H2O.max(0.0);
 
+        populationBalance->solve();
+
+        runTime.write();
         runTime.printExecutionTime(Info);
     }
 
@@ -170,6 +195,5 @@ int main(int argc, char *argv[])
 
     return 0;
 }
-
 
 // ************************************************************************* //
